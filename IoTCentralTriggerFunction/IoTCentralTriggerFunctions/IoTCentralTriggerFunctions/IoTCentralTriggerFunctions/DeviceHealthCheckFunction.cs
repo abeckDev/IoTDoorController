5de4a8f0c834e070c4e40cf72392021b5fc0e323
Alexander Beck <alexander.beck@microsoft.com>
2020-06-14 22:11:50 +0200

    Added a new Azure Function Added a Function which will check a Device in IoT Central and update its status in IoT Central

---
+++ IoTCentralTriggerFunction/IoTCentralTriggerFunctions/IoTCentralTriggerFunctions/IoTCentralTriggerFunctions/DeviceHealthCheckFunction.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace IoTCentralTriggerFunctions
+{
+    public static class DeviceHealthCheckFunction
+    {
+        [FunctionName("DeviceHealthCheckFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (req.Method != HttpMethod.Get.ToString())
+            {
+                //Send an Error if wrong HTTP method is used
+
+                return new BadRequestObjectResult("Bad Request");
+            }
+            string door;
+            // parse query parameter
+            try
+            {
+                door = req.GetQueryParameterDictionary()["door"];
+            }
+            catch (Exception)
+            {
+                return new BadRequestObjectResult("Bad Request");
+            }
+
+
+            log.LogInformation("Received request to check door " + door);
+
+            using (var client = new HttpClient())
+            {
+                string IoTAppName = Environment.GetEnvironmentVariable("IoTAppName");
+                string IoTDeviceName = Environment.GetEnvironmentVariable("IoTDeviceName");
+                string IoTComponentName = Environment.GetEnvironmentVariable("IoTComponentName");
+                bool isConvertibleToInt = Int32.TryParse(Environment.GetEnvironmentVariable("IoTDeviceTimeoutInMinutes"), out int IoTDeviceTimeoutInMinutes);
+                string IoTCoreAccessToken = Uri.EscapeDataString(Environment.GetEnvironmentVariable("IoTCentralToken"));
+
+                if (!isConvertibleToInt)
+                {
+                    return new BadRequestObjectResult("Error in Config");
+                }
+
+                string ApiUrl = $"https://{IoTAppName}.azureiotcentral.com/api/preview/devices/{IoTDeviceName}/components/{IoTComponentName}/properties?access_token={IoTCoreAccessToken}";
+                var response = await client.GetAsync(ApiUrl);
+                var content = await response.Content.ReadAsStringAsync();
+
+                JObject responseJson = JObject.Parse(content);
+                DateTime lastHealthcheck = responseJson["$metadata"]["DeviceStatus"]["ackLastUpdatedTimestamp"].ToObject<DateTime>();
+                TimeSpan lastUpdateDiff = DateTime.Now.Subtract(lastHealthcheck);
+                if (lastUpdateDiff.Days >= 1 || lastUpdateDiff.Minutes > IoTDeviceTimeoutInMinutes)
+                {
+                    //Device is Timed out
+                    //Set Device to Offline in IoT Central
+                    ApiUrl = $"https://{IoTAppName}.azureiotcentral.com/api/preview/devices/{IoTDeviceName}/cloudProperties?access_token={IoTCoreAccessToken}";
+                    var body = new StringContent("{\"DeviceStatus\": \"offline\"}", Encoding.UTF8, "application/json");
+                    var debug = await client.PutAsync(ApiUrl, body);
+                    return new OkObjectResult("Offline");
+                }
+                //Set Device to online in IoT Central
+                ApiUrl = $"https://{IoTAppName}.azureiotcentral.com/api/preview/devices/{IoTDeviceName}/cloudProperties?access_token={IoTCoreAccessToken}";
+                var data = new StringContent("{\"DeviceStatus\": \"ready\"}", Encoding.UTF8, "application/json");
+                await client.PutAsync(ApiUrl, data);
+                return new OkObjectResult("Ok");
+            }
+        }
+    }
+}
